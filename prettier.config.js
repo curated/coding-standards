4073dff22478fe245f9feb9939f2b9ed04686b75
Wagner Camarao <wagner@camarao.io>
2018-02-10 20:58:06 -0200

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  insertPragma: false,
+  parser: 'typescript',
+  printWidth: 80,
+  proseWrap: 'always',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
